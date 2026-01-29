3e5b759d2d528c8601f8159577b67e7665898488
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-01-29 17:46:08 +0000

    Table Creation Done

--- db/data/development-data/index.js
+++ db/data/development-data/index.js
@@ -1,4 +1,12 @@
-exports.articleData = require('./articles.js');
-exports.commentData = require('./comments.js');
-exports.topicData = require('./topics.js');
-exports.userData = require('./users.js');
+exports.articleData = require("./articles.js");
+exports.commentData = require("./comments.js");
+exports.topicData = require("./topics.js");
+exports.userData = require("./users.js");
+
+async function getAnimals() {
+  try {
+    await pool.query("SELECT * FROM animals;");
+  } catch (err) {
+    console.error("Database Error: ", err);
+  }
+}
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,68 @@
-const db = require("../connection")
+const db = require("../connection");
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+const seed = async ({ topicData, userData, articleData, commentData }) => {
+  /*STEP 1
+DROP tables in reverse order to ensure idempotency (in comparison to creation order below)
+We drop them in reverse because a parent table cannot be deleted while its
+PK is still being referenced by a FK in a child table */
+
+  await db.query(/* SQL */ `
+    DROP TABLE IF EXISTS comments;
+    DROP TABLE IF EXISTS articles;
+    DROP TABLE IF EXISTS topics;
+    DROP TABLE IF EXISTS users;`);
+
+  /*STEP 2
+CREATE TABLES starting with parents because children cannot be created
+until their FKs have the PKs to validate against*/
+
+  await db.query(
+    /* SQL */
+    `CREATE TABLE users (
+      username VARCHAR NOT NULL, 
+      name VARCHAR NOT NULL, 
+      avatar_url VARCHAR, 
+      CONSTRAINT pk_users PRIMARY KEY (username));`
+  );
+
+  await db.query(
+    /* SQL */
+    `CREATE TABLE topics (
+      slug VARCHAR, 
+      description VARCHAR NOT NULL, 
+      img_url VARCHAR, 
+      CONSTRAINT pk_slug PRIMARY KEY (slug));`
+  );
+
+  await db.query(
+    /* SQL */
+    `CREATE TABLE articles (
+      article_id SERIAL, 
+      title VARCHAR NOT NULL, 
+      topic VARCHAR NOT NULL, 
+      author VARCHAR NOT NULL, 
+      body TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      votes INT DEFAULT 0,
+      article_img_url VARCHAR,
+      CONSTRAINT pk_article_id PRIMARY KEY (article_id),
+      CONSTRAINT fk_topic FOREIGN KEY (topic) REFERENCES topics(slug),
+      CONSTRAINT fk_author FOREIGN KEY (author) REFERENCES users(username));`
+  );
+
+  await db.query(
+    /* SQL */
+    `CREATE TABLE comments (
+      comment_id SERIAL, 
+      article_id INT NOT NULL, 
+      body TEXT NOT NULL, 
+      votes INT DEFAULT 0, 
+      author VARCHAR NOT NULL, 
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+      CONSTRAINT pk_comment_id PRIMARY KEY (comment_id),
+      CONSTRAINT fk_article_id FOREIGN KEY (article_id) REFERENCES articles(article_id),
+      CONSTRAINT fk_author FOREIGN KEY (author) REFERENCES users(username));`
+  );
 };
+
 module.exports = seed;
