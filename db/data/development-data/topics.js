5c71ae679faf912d1c0d3b20b068e74c17a89a2e
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-02-27 13:01:04 +0100

    update seed data for production

--- db/data/development-data/topics.js
+++ db/data/development-data/topics.js
@@ -3,13 +3,16 @@ module.exports = [
   {
     description: "FOOTIE!",
     slug: "football",
-    img_url:
-      "https://images.pexels.com/photos/209841/pexels-photo-209841.jpeg?w=700&h=700",
+    img_url: "https://www.svgrepo.com/show/525901/football.svg",
   },
   {
     description: "Hey good looking, what you got cooking?",
     slug: "cooking",
-    img_url:
-      "https://images.pexels.com/photos/33242/cooking-ingredient-cuisine-kitchen.jpg?w=700&h=700",
+    img_url: "https://www.svgrepo.com/show/401342/cooking.svg",
+  },
+  {
+    description: "We love coding",
+    slug: "coding",
+    img_url: "https://www.svgrepo.com/show/474334/coding.svg",
   },
 ];
